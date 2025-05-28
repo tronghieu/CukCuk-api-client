6a63520399f95241e09da9bb2db687b941c70ea3
Lưu Trọng Hiếu <tronghieu.luu@gmail.com>
2025-05-28 14:09:43 +0700

    Reexport types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { CukCukClient } from './client';
 import type { ClientConfig } from './types';
 
+export * from './types';
 export { CukCukClient };
 export type { ClientConfig };
 export default CukCukClient; 
